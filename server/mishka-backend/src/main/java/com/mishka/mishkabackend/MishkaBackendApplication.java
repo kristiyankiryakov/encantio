306a061e26dc726c4f051e00e37fed836da67eaf
Georgi Kirov <georgipetrovkirov@gmail.com>
2024-04-20 22:28:45 +0300

    init server

---
+++ server/mishka-backend/src/main/java/com/mishka/mishkabackend/MishkaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mishka.mishkabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MishkaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MishkaBackendApplication.class, args);
+	}
+
+}
---
+++ server/mishka-backend/src/test/java/com/mishka/mishkabackend/MishkaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mishka.mishkabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MishkaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
