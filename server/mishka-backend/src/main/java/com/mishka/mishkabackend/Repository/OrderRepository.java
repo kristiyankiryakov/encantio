2892ec05cd5609c6043ad1b4402289b53c80b816
Kristiyan Kiryakov <126320553+kristiyankiryakov@users.noreply.github.com>
2024-04-21 17:41:04 +0300

    Fix #2 repository name & anotation

---
+++ server/mishka-backend/src/main/java/com/mishka/mishkabackend/Repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.mishka.mishkabackend.Repository;
+
+import com.mishka.mishkabackend.Entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+}
