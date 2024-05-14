93cd48f324e1b0263e78e0799bafdaac7ac00693
Kristiyan Kiryakov <126320553+kristiyankiryakov@users.noreply.github.com>
2024-05-14 13:17:08 +0300

    style product card

--- client/src/landing/Main/ProductCard.tsx
+++ client/src/landing/Main/ProductCard.tsx
@@ -6,7 +6,7 @@ type Props = {}
 
 function ProductCard({ }: Props) {
     return (
-        <div className='z-20 flex flex-col gap-10 w-[27%] p-4 rounded-md border border-1 border-gray-200/20  bg-cardBackground relative '>
+        <div className='z-20 flex flex-col flex-wrap gap-10 w-[30%] xs:p-1 md:p-2 xl:p-4 rounded-md border border-1 border-gray-200/20  bg-cardBackground relative  xs:text-xs sm:text-sm'>
 
             <div className='Feet image select-none w-6/12 z-20'>
                 <img className='w-full' src={feet} alt="mouse-feet" />
@@ -19,24 +19,24 @@ function ProductCard({ }: Props) {
 
             <div className='flex flex-col gap-5'>
 
-                <div className='product-title text-white font-medium text-2xl tracking-wider'>
+                <div className='product-title text-white font-medium xs:text-base sm:text-base md:text-base xl:text-xl 2xl:text-2xl tracking-wider'>
                     <p>Logitech G Pro X</p>
                 </div>
 
-                <div className='product-description text-customGray w-5/6'>
+                <div className='product-description text-customGray 2xl:w-5/6'>
                     <p> Прецизни и издръжливи крачета, специално проектирани за плавно движение и бързи реакции.</p>
                 </div>
 
-                <div className='flex flex-wrap text-customGray gap-2' >
+                <div className='flex flex-wrap text-customGray xs:gap-1 lg:gap-2' >
                     <div className='tag px-3 py-1 bg-gray-800 rounded-md'>Logitech</div>
                     <div className='tag px-3 py-1 bg-gray-800 rounded-md'>G Pro X</div>
                     <div className='tag px-3 py-1 bg-gray-800 rounded-md'>Mouse feet</div>
                 </div>
 
-                <div className='pricing-and-offer flex items-center'>
-                    <p className='price text-3xl text-white font-medium' >14 лв.</p>
+                <div className='pricing-and-offer xs:flex-col xl:flex-row flex items-center justify-between xs:w-full'>
+                    <p className='price text-white' ><span className='pr-1 xs:text-lg font-medium sm:' >14</span>лв.</p>
 
-                    <p className='offer-description text-customGray' >за единичен сет крачета</p>
+                    <p className='offer-description text-customGray w-full xs:text-center xl:text-left' >за единичен сет крачета</p>
                 </div>
 
                 <div className='bg-gray-700 w-full h-[0.065rem]' >&nbsp;</div>
@@ -45,10 +45,10 @@ function ProductCard({ }: Props) {
 
             </div>
 
-            <div className='absolute -right-5 z-20' >
+            <div className='absolute -right-5 z-20 xs:w-[4rem] sm:w-[5rem] md:w-[6rem] lg:w-[7rem] xl:w-[8rem] 2xl:w-[10rem]' >
                 <img className='w-full' src={mouse} alt="mouse" />
             </div>
-            
+
             {/* <div className='absolute top-0 h-[10rem] w-[10rem] left-44 z-0 rounded-full bg-radial-gradient-product opacity-15' ></div> */}
 
         </div>
